4e89c61250fdce2b695d378b82b5f03af1b89fb2
Sherley Jones <sherleyjones@sherleyonessmbp.attlocal.net>
2014-12-04 23:57:59 -0600

    working

--- PasswordCrack.java
+++ PasswordCrack.java
@@ -5,6 +5,7 @@ import java.math.*;
 
 public class PasswordCrack {
 	public static Hashtable<String,Vector<String>> dictMangles = new Hashtable<String,Vector<String>>();
+	public static Hashtable<String,Vector<String>> dictDoubleMangles = new Hashtable<String,Vector<String>>();
 
 	public static void main(String args[]) throws IOException{
 
@@ -19,52 +20,19 @@ public class PasswordCrack {
 			BufferedReader pswReader = new BufferedReader(pswStream);
 
 			String psw_cracked = null;
-			
-			String word = new String();
-			while((word = dictReader.readLine()) != null){
-				Vector<String> v = new Vector<String>();
-
-				// append char to word.
-				for (int i = 33; i < 127; i++) {
-					v.add(word + ((char) i));
-				}
-			
-				//prepend char to word
-				for (int i = 33; i < 127; i++) {
-					v.add(((char) i) + word);
-				}
-
-				//all uppercase
-				v.add(word.toUpperCase());
-				
-
-				//uppercase first letter
-				int wordInt = word.chatAt(0);
-				v.add(((char) i) + word);
-
-				//remove first letter
-				v.add(word.substring(1, word.length()) );
-
-				//remove last letter
-				v.add(word.substring(0, word.length() - 1));
-
-				//prepend char to word and uppercase first letter
-				for (int i = 33; i < 127; i++) {
-					v.add(((char) i) + word);
-				}
-
-				//System.out.println(dictMangles.get(word));
-				dictMangles.put(word, v);
-				v.clear();
-			}
+			buildHashTable(dictReader);
 
 			String psw_line = new String();
+			psw_line = pswReader.readLine();
 			while((psw_line = pswReader.readLine()) != null){
 				System.out.println(psw_line);
 				String[] user = psw_line.split(":");
 				
 				psw_cracked = crack(user);
-				//System.out.println(psw_cracked);
+				if (psw_cracked != null)
+					System.out.println("CRACKED! : " + psw_cracked);
+				else
+					System.out.println("not cracked :(");
 			}
 
 		} catch (Exception E) {
@@ -76,41 +44,134 @@ public class PasswordCrack {
 	public static String crack(String[] user){
 		String salt = user[1].substring(0,2);
 		String pswE = user[1].substring(2);
+		String pswESalt = user[1];
 		String[] name = user[4].split(" ");
-		// System.out.println(name[0] + name[1]);
-		// System.out.println(user[1]);
 		boolean match = false;
 		String crackedPsw = null;
+		String wordE = new String();
+		String key = new String();
 		int comp;
 
-		// try{
-		// 	String word = new String();
-		// 	while (((word = dictReader.readLine()) != null)){
-				
-		// 		String wordE = jcrypt.crypt(salt, word);
-		// 		if( (comp = wordE.compareTo(pswE)) == 0) {
-		// 			match = true;
-		// 			crackedPsw = word;
-		// 			break;
-		// 		}
-		// 		String temp = new String();
-  //     			if( (temp = pre_append(word, salt, pswE)) != null){
-  //     				match = true;
-		// 			crackedPsw = word;
-		// 			break;
-  //     			}
-		// 	}
-		// } catch (Exception E) {
-		// 	System.out.println("Caught Exception: " + E);}
+		Enumeration<String> keys = dictMangles.keys();
+		while (keys.hasMoreElements() && !match) {
+			key = keys.nextElement();
 
+			wordE = jcrypt.crypt(salt, key);
+			if( (comp = pswESalt.compareTo(wordE)) == 0 ){
+				crackedPsw = key;
+				match = true;
+				break;
+			}
+			wordE = jcrypt.crypt(salt, name[0].toLowerCase());
+			if( (comp = pswESalt.compareTo(wordE)) == 0){
+				crackedPsw = name[0];
+				match = true;
+				break;
+			}
+
+			wordE = jcrypt.crypt(salt, name[1].toLowerCase());
+			if( (comp = pswESalt.compareTo(wordE)) == 0 ){
+				crackedPsw = name[1];
+				match = true;
+				break;
+			}
+
+			Vector<String> wordList = dictMangles.get(key);
+			//System.out.println("hi");
+			for(int i = 0; i < wordList.size(); i++){
+				String word = wordList.get(i);
+				//System.out.println(word);
+				wordE = jcrypt.crypt(salt, word);
+				if( (comp = pswESalt.compareTo(wordE)) == 0 ){
+					crackedPsw = word;
+					match = true;
+					break;
+				}
+			}
+
+
+			// System.out.println(key + " : " + dictMangles.get(key));	
+			// System.out.println();
+			// System.out.println();
+			// System.out.println();	
+		} 
+		
 		return crackedPsw;
 	}
+
+	// public static boolean WordEncryptCheck(){
+
+	// }
 
 	public static String pre_append (String word, String salt, String pswE){
 		String pswCracked = null;
 		String temp = new String();
 
 		return pswCracked;
+	}
+
+	public static void buildHashTable(BufferedReader dictReader){
+		String word = new String();
+		String temp = new String();
+
+		try{
+			while((word = dictReader.readLine()) != null){
+				int wordInt = (int)word.charAt(0);
+				Vector<String> v = new Vector<String>();
+				Vector<String> vDouble = new Vector<String>();
+				// //temp = word;
+
+				//all uppercase
+				v.add(word.toUpperCase());
+
+				//uppercase first letter
+				v.add( ((char)((wordInt>96 && wordInt<123)? wordInt - 32: wordInt)) + word.substring(1, word.length()) );
+
+				//remove first letter
+				v.add(word.substring(1, word.length()) );
+
+				//remove last letter
+				v.add(word.substring(0, word.length() - 1));
+
+				//duplicate string
+				v.add(word.concat(word));
+
+				//ncapitalize the string
+				temp = word.substring(1, word.length()).toUpperCase();
+				v.add(((char)((wordInt>96 && wordInt<123)? wordInt - 32: wordInt)) + temp);
+
+				// append char to word.
+				for (int i = 33; i < 127; i++) {
+					v.add(word + ((char) i));
+				}
+			
+				//prepend char to word
+				for (int i = 33; i < 127; i++) {
+					v.add(((char) i) + word);
+				}
+				
+				//reverse string
+				String rev = new String();
+				for (int i = 0; i < word.length(); i++) {
+					rev += word.charAt(word.length() - 1 - i);
+				}
+				v.add(rev);
+				//System.out.println(rev);
+
+				//prepend char to word and uppercase first letter
+				temp = ((char)((wordInt>96 && wordInt<123)? wordInt - 32: wordInt)) + word.substring(1, word.length());
+				for (int i = 33; i < 127; i++) {
+					vDouble.add(((char) i) + temp);
+				}
+
+				//System.out.println(dictMangles.get(word));
+				dictMangles.put(word, v);
+				dictDoubleMangles.put(word, vDouble);
+				//System.out.println(word + " : " + dictMangles.get(word));
+				//v.clear();
+			}
+		} catch (Exception E) {
+			System.out.println("Caught Exception: " + E);}
 	}
 
 
